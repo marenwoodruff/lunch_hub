7107c3eafb774f1d83e095b1175115443a47a784
MarenWoodruff <wren403@gmail.com>
2017-02-01 12:56:13 -0500

    added seed file

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-    scope '/api' do
+    # scope '/api' do
         resources :groups, except: [:new, :edit]
-    end
+    # end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Group.create([
+  { name: 'Ben Franklin Labs' },
+  { name: 'Snip Salon Software' },
+  { name: 'GloboChem' },
+  { name: 'TechCorp' },
+])
